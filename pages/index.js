85707a0b3aab21be05ec163a6295ca4f5ed14a20
yuxxeun <itsyuxxeun@gmail.com>
2022-07-10 03:04:58 +0700

    deps: chakra-ui/icon

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Fade, ScaleFade, Slide, SlideFade, Text} from '@chakra-ui/react'
 import styles from '../styles/Home.module.css'
+import { PhoneIcon } from '@chakra-ui/icons'
 
 export default function Home() {
   return (
@@ -15,6 +17,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+          <Text fontSize="6xl">Next</Text>
+<PhoneIcon color="red.500" />
+
+	
 
         <p className={styles.description}>
           Get started by editing{' '}
